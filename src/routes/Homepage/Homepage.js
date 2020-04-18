26a990ecb9030144fb2714e47167c1f72fe56bc9
stevenwthornton0 <stevenwthornton0@gmail.com>
2020-04-18 12:23:13 -0500

    updated about me info

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -14,20 +14,20 @@ class Homepage extends Component {
             <section className='bio'>
                 <div className='container'>
                     <a href="https://virtualpiano.net/" target='_blank' rel="noopener noreferrer">Piano Player</a>
-                    <a href="https://www.nps.gov/mora/index.htm" target='_blank' rel="noopener noreferrer">Nature Lover</a>
+                    <a href="https://www.nps.gov/mora/index.htm" target='_blank' rel="noopener noreferrer">Nature Admirer</a>
                     <a href="https://www.thinkful.com/" target='_blank' rel="noopener noreferrer">Thinkful Grad</a>
                 </div>
 
                 <div className='bioContainer'>
-                    <h2 className='text1'>Hello, I’m Steven! I’m a full-stack web developer and I absolutely love creating websites</h2>
+                    <h2 className='text1'>Hello, I’m Steven! I’m a Full-Stack Web Developer and I am passionate about creating web applications both user-friendly and beautiful.</h2>
 
                     <div className='weddingPhoto1'>
                         <img src={Photo} alt='Piano keys'/>
                     </div>
 
-                    <p className='text2'>I am a recent graduate from the Full-Stack Flex bootcamp at Thinkful. To me coding is like putting together a puzzle. You take the pieces that you have been given and put them together in a unique, fun, and satisfying way. You can create something not only beautiful but extremely useful for the consumer. I specifically love the front-end side of development. Designing a beautiful, user-friendly, and responsive webpage is extremely satisfying</p>
+                    <p className='text2'>I am a certified Full-Stack web developer and graduate from the Full-Stack Flex program at the Thinkful/Bloc online bootcamp. I love to find creative, technical solutions for life's every-day complex problems. During my apprenticeship at Thinkful/Bloc, I built two full-stack applications from the ground up using the Node.js environment. These projects can be found on the Projects page. Before my apprenticeship, I worked in various customer service roles, as well as a year administrating a network of internships around the United States.</p>
 
-                    <p className='mobile text3'>When I’m not coding you can find me serving at my local church, on the golf course attempting to shave off a few strokes, fishing, playing piano, or on the couch with my wife watching a movie. I love to learn and grow, taking everything I do to the next level including my skill in code. I bring excitement and dedication in anything I put my hands to. My lifelong dream is to raise a family with my wife and see our kids have children of their own.</p>
+                    <p className='mobile text2'>When I’m not coding you can find me serving at my local church, on the golf course attempting to shave off a few strokes, fishing, playing piano, or on the couch with my wife watching a movie. I love to learn and grow, taking everything I do to the next level including my skill in code. I bring excitement and dedication in anything I put my hands to. My lifelong dream is to raise a family with my wife and see our kids have children of their own.</p>
 
 
                     <div className='languageIcons'>
